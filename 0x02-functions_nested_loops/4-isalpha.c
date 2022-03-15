d165370b3fc507c25e828f66b3d8e1925da96c29
FekaduDuguma <fekadu.duguma.woltejji@gmail.com>
2022-03-15 23:46:07 +0300

    isalpha 2

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -4,7 +4,7 @@
  * _isalpha - returns 1 if lowercase alphabet and 0 otherwise
  * @c: take in an character
  * Return: 1 for letter or 0 otherwise
- * */
+ **/
 int _isalpha(int c)
 {
 	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
