f702fe24045dd44d1ce05e73bb1d5bcc78c4e326
FekaduDuguma <fekadu.duguma.woltejji@gmail.com>
2022-03-15 21:04:06 +0300

    compiled o-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
- * main-prints _putchar, followed by a new line
- * Return:Always 0 (Success)
+ * main -  prints _putchar, followed by a new line
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
 	_putchar('_putchar');
 	_putchar('\n');
-	return(0);
 
+	return (0);
 }
