ccf8272b2950b2d071793c195ee7ac749529c173
FekaduDuguma <fekadu.duguma.woltejji@gmail.com>
2022-03-17 20:42:17 +0300

    triangles

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * main- print_triangles prints a triangle
+ * use the character #
+ */
+void print_triangle(int size)
+{
+	if (size <=0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int i, j;
+
+		for (i =1; i <= size; i++)
+		{
+			for (j =i; j <=size; j++)
+			{
+				_putchar(' ');
+			}
+			for (j =i; j <=size; j++)
+			{
+				_putchar('#');
+			}	
+			_putchar('\n');
+		}
+	}
+}
